107fe8035c4b02cd7a9680fb40185827ca13dcfd
akaxd <1073432021@qq.com>
2017-09-11 21:11:54 +0800

    讲师列表渲染-完成

--- src/js/teacher/list.js
+++ src/js/teacher/list.js
@@ -3,6 +3,14 @@ require('../common/asdie.js');
 
 
 
-$('#editTc').on("click", function() {
-
+$.get('/v6/teacher', function(data) {
+    $('#teacher-template').html(template('teacher-list-tpl', data.result))
 })
+
+/**
+ * 讲师启用与注销
+ * 
+ * 1.因为按钮是随着列表动态渲染生成的,所以必须要使用事件委托,不然绑定不到
+ * 2.通过自定义属性点击时拿到按钮身上的接口
+ * 3.请求接口
+ */
