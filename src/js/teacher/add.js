0b86614cef3121a09296cd820b2f3aed4df1fc27
akaxd <1073432021@qq.com>
2017-09-11 21:10:49 +0800

    添加讲师-完成

--- src/js/teacher/add.js
+++ src/js/teacher/add.js
@@ -1,2 +1,10 @@
 require('../common/header.js');
 require('../common/asdie.js');
+$('#teacher-add-Form').ajaxForm({
+    // delegation: true,
+    success: function(data) {
+        if (data.code == 200) {
+            alert('恭喜打开新世界大门')
+        }
+    }
+})
