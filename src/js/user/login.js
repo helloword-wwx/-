83dbc372e43d2f4b34fd1fa52b756f95fd06c61e
akaxd <1073432021@qq.com>
2017-09-11 10:21:08 +0800

    首页登录完成

---
+++ src/js/user/login.js
@@ -0,0 +1,37 @@
+//当英胡点击登录按钮的时候.这个插件ajaxForm会自动监听submit事件
+//然后阻止浏览器默认的刷新提交,然后自动变成ajax的方式发送请求.
+$('#login-form').ajaxForm({
+    success: function(data) {
+        if (data.code == 200) {
+            alert('登录成功,笑嘻嘻');
+            localStorage.setItem('userinfo', JSON.stringify(data.result))
+            location.href = '/dist'
+        } else {
+            alert('登录失败,妈卖批')
+        }
+    }
+});
+// console.log(78)
+
+
+
+// $('#login-form').on('submit', function() {
+//     $.ajax({
+//         url: '/v6/login',
+//         type: 'post',
+//         data: $(this).serialize(),
+//         success: function() {
+//             if (data.code == 200) {
+//                 alert('登录成功,笑嘻嘻')
+//             } else {
+//                 alert('登录失败,妈卖批')
+//             }
+//         },
+//         error: function() {
+//             alert('登录名不存在或密码错误')
+//         },
+
+//     });
+
+//     return false; //jq中阻止浏览器默认事件 return false就好了.
+// });
