774c06e0d563c9346ef410eea9c0bdd51d29a1ba
akaxd <1073432021@qq.com>
2017-09-11 11:34:00 +0800

    学科列表渲染-完成

--- src/js/category/list.js
+++ src/js/category/list.js
@@ -1,2 +1,6 @@
 require('../common/header.js');
 require('../common/asdie.js');
+
+$.get('/v6/category', function(data) {
+    $('.table-bordered').append(template('category-list-tal', data.result));
+})
